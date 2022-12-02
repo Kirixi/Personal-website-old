c119df6e9c4c7ea3243c97e9d9d1d9a95db40142
s3813866 <tehyeuhaw@gmail.com>
2022-12-03 03:03:53 +1100

    Navbar removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
