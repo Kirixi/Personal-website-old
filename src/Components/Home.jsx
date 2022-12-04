5a585ee577a26d4d6bd57581c960d835e13b8519
s3813866 <tehyeuhaw@gmail.com>
2022-12-05 02:32:56 +1100

    Added new font style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const Home = () => {
-  return <div className='w-full h-screen bg-[#04038f]'></div>;
+  return (
+    <div name='home' className='w-full h-screen bg-[#04038f]'>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className=' text-gray-300'>Hi, My name is YeuHaw Teh or Jonathan </p>
+      </div>
+    </div>
+  );
 };
