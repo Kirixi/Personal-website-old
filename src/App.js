da70c2a2b777098778e8f918c369cd594fbe187a
s3813866 <tehyeuhaw@gmail.com>
2022-12-13 01:48:53 +1100

    Experience section title added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { Home } from './Components/Home';
+import Skils from './Components/Skils';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Skils />
     </div>
   );
 }
---
+++ src/Components/Skils.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Skils = () => {
+  return (
+    <div className='bg-[#04038f] text-gray-200'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-red-600 '>
+            Experience
+          </p>
+          <h2>Technologies I have worked with</h2>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Skils;
