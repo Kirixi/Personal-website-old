3ec3312d2c1e101376a70fc8e68528e3564c98b8
s3813866 <tehyeuhaw@gmail.com>
2022-12-30 15:58:38 +0800

    Project section component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { Home } from './Components/Home';
 import Skils from './Components/Skils';
+import Projects from './Components/Projects';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <Skils />
+      <Projects />
     </div>
   );
 }
