0d63f98442687933f22c6205e12d98e711ed581d
s3813866 <tehyeuhaw@gmail.com>
2023-01-01 18:13:54 +0800

    Project section title and description implemented

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className='w-full md:h-screen bg-[#04038f] text-gray-200'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-red-600 '>
+            Projects
+          </p>
+          <h2 className='py-4 text-2xl'>
+            Check out some of the from my past:{' '}
+          </h2>
+        </div>
+        //here
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
