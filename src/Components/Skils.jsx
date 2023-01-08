555ff6105b08f5d9a3620d54a206aa8ba8069795
s3813866 <tehyeuhaw@gmail.com>
2023-01-08 14:38:58 +0800

    first production commit

--- src/Components/Skils.jsx
+++ src/Components/Skils.jsx
@@ -13,7 +13,7 @@ import CCI from '../images/circleci.png';
 
 const Skils = () => {
   return (
-    <div name='skills' className='bg-[#04038f] text-gray-200'>
+    <div name='skills' className='bg-[#04038f] text-gray-200 h-screen'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-red-600 '>
@@ -21,97 +21,101 @@ const Skils = () => {
           </p>
           <h2 className='py-4 text-2xl'>Technologies I have worked with: </h2>
         </div>
+        <br />
+        <div className='flex flex-wrap space-x-4 gap-y-3 justify-center'>
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={JS}
+              alt='Javascript icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={CSHARP}
+              alt='C# icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm bg-slate-200'
+              src={JAVA}
+              alt='Java icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 bg-white shadow-sm'
+              src={NODE}
+              alt='Node icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={CPP}
+              alt='C++ icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={PY}
+              alt='Python icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={RE}
+              alt='React icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={TW}
+              alt='Tailwind icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={AWS}
+              alt='AWS icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={DOCKER}
+              alt='Docker icon'
+            />
+          </div>
+
+          <div class='w-24 h-24'>
+            <img
+              class='rounded-full border border-gray-100 shadow-sm'
+              src={CCI}
+              alt='CircleCI icon'
+            />
+          </div>
+        </div>
       </div>
 
-      <div class=' max-w-[1000px] mx-auto p-4 w-full flex flex-wrap space-x-4 gap-y-3 justify-center '>
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={JS}
-            alt='Javascript icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={CSHARP}
-            alt='C# icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm bg-slate-200'
-            src={JAVA}
-            alt='Java icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 bg-white shadow-sm'
-            src={NODE}
-            alt='Node icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={CPP}
-            alt='C++ icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={PY}
-            alt='Python icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={RE}
-            alt='React icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={TW}
-            alt='Tailwind icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={AWS}
-            alt='AWS icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={DOCKER}
-            alt='Docker icon'
-          />
-        </div>
-
-        <div class='w-24 h-24'>
-          <img
-            class='rounded-full border border-gray-100 shadow-sm'
-            src={CCI}
-            alt='CircleCI icon'
-          />
-        </div>
-      </div>
+      {/* <div class=' max-w-[1000px] mx-auto p-4 w-full  '>
+        
+      </div> */}
     </div>
   );
 };
